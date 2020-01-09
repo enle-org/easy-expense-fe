654b15909afcf1d35d7b4fa82abcd1f17061b559
Ekpo David <dave170e71@gmail.com>
2020-01-09 18:30:36 +0100

    hamburger, responsiveness

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { push as Menu } from 'react-burger-menu';
 import { WithContext as ReactTags } from 'react-tag-input';
+import Link from 'next/link';
 import Nav from './Nav';
 
 const KeyCodes = {
@@ -35,73 +37,103 @@ class Dashboard extends React.Component {
   render() {
     const { tags } = this.state;
     return (
-      <div className="accountWrapper">
-        <Nav />
-        <main className="accountWrapper__main">
-          <h1 className="accountPageTitle">Dashboard</h1>
-          <div className="u_section head">
-            <h2 className="pageTitle">Getting Started</h2>
-            <p>
-              You can create an organisation and start reviewing expense reports as an
-              expense approver or join an organization via an invite link.
-            </p>
+      <div id="outer-container" className="dashboard-outer">
+        <Menu right pageWrapId="page-wrap" outerContainerId="outer-container">
+          <div>
+            <Link href="/dashboard" as="/dashboard">
+              <a>Dashbaord</a>
+            </Link>
+            <Link href="/" as="/">
+              <a>Receipts</a>
+            </Link>
+            <Link href="/" as="/">
+              <a>Expenses</a>
+            </Link>
+            <Link href="/" as="/">
+              <a>Expense Report</a>
+            </Link>
+            <Link href="/dashboard/settings" as="/dashboard/settings">
+              <a>Settings</a>
+            </Link>
           </div>
-          <div className="body">
-            <div className="u_section u_card">
-              <h4 className="sectionTitle">Organization Expense Approver</h4>
-              <form action="#" className="eEForm eEForm__row eEForm__account">
-                <div className="eERow2">
-                  <div className="formGroup">
-                    <label htmlFor="organizationName">Organization Name</label>
-                    <input
-                      type="text"
-                      name="organizationName"
-                      id="organizationName"
-                      placeholder="Enter organization name"
-                      defaultValue="Enle Co"
-                    />
-                  </div>
-                  <div className="formGroup">
-                    <label htmlFor="members">Members (Add emails seperated by a comma)</label>
-                    <ReactTags
-                      placeholder="Add a member"
-                      tags={tags}
-                      allowDragDrop={false}
-                      handleDelete={this.handleDelete}
-                      handleAddition={this.handleAddition}
-                      delimiters={delimiters}
-                    />
-                  </div>
+        </Menu>
+        <div id="page-wrap">
+          <div className="accountWrapper">
+            <header className="u_section">
+              <div className="brand">
+                <Link href="/dashboard" as="/dashboard">
+                  <img src="/logo.png" alt="Easy Expense logo" />
+                </Link>
+              </div>
+            </header>
+            <Nav />
+            <main className="accountWrapper__main">
+              <h1 className="accountPageTitle">Dashboard</h1>
+              <div className="u_section head">
+                <h2 className="pageTitle">Getting Started</h2>
+                <p>
+                  You can create an organisation and start reviewing expense reports as an
+                  expense approver or join an organization via an invite link.
+                </p>
+              </div>
+              <div className="body">
+                <div className="u_section u_card">
+                  <h4 className="sectionTitle">Organization Expense Approver</h4>
+                  <form action="#" className="eEForm eEForm__row eEForm__account">
+                    <div className="eERow2">
+                      <div className="formGroup">
+                        <label htmlFor="organizationName">Organization Name</label>
+                        <input
+                          type="text"
+                          name="organizationName"
+                          id="organizationName"
+                          placeholder="Enter organization name"
+                          defaultValue="Enle Co"
+                        />
+                      </div>
+                      <div className="formGroup">
+                        <label htmlFor="members">Members (Add emails seperated by a comma)</label>
+                        <ReactTags
+                          placeholder="Add a member"
+                          tags={tags}
+                          allowDragDrop={false}
+                          handleDelete={this.handleDelete}
+                          handleAddition={this.handleAddition}
+                          delimiters={delimiters}
+                        />
+                      </div>
+                    </div>
+                    <div className="buttonWrapper">
+                      <button type="submit" className="button button__primary">
+                        Create organization
+                      </button>
+                    </div>
+                  </form>
                 </div>
-                <div className="buttonWrapper">
-                  <button type="submit" className="button button__primary">
-                    Create organization
-                  </button>
+                <div className="u_card">
+                  <h4 className="sectionTitle">Organization Member</h4>
+                  <form action="#" className="eEForm eEForm__account">
+                    <div className="formGroup">
+                      <label htmlFor="inviteLink">Invite link</label>
+                      <input
+                        type="text"
+                        name="inviteLink"
+                        id="inviteLink"
+                        value="https://www.easyexpense.com/invite/Enle%20Co"
+                        disabled
+                      />
+                    </div>
+                    <div className="buttonWrapper">
+                      <button type="submit" className="button button__primary">
+                        Join Organization
+                      </button>
+                    </div>
+                  </form>
                 </div>
-              </form>
-            </div>
-            <div className="u_card">
-              <h4 className="sectionTitle">Organization Member</h4>
-              <form action="#" className="eEForm eEForm__account">
-                <div className="formGroup">
-                  <label htmlFor="inviteLink">Invite link</label>
-                  <input
-                    type="text"
-                    name="inviteLink"
-                    id="inviteLink"
-                    value="https://www.easyexpense.com/invite/Enle%20Co"
-                    disabled
-                  />
-                </div>
-                <div className="buttonWrapper">
-                  <button type="submit" className="button button__primary">
-                    Join Organization
-                  </button>
-                </div>
-              </form>
-            </div>
+              </div>
+            </main>
           </div>
-        </main>
+        </div>
       </div>
     );
   }
