b7f7499b4dd8036bb0ab17c5530eb020ba84036b
nedyudombat <nedyudombat@gmail.com>
2020-01-28 14:52:40 +0100

    password recovery flow

---
+++ pages/recovery/[id].js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Head from 'next/head';
+import Reset from '../../src/components/auth/Reset';
+
+const RecoveryPage = () =>  {
+  return (
+    <div>
+      <Head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+        <title>Password Reset | Easy Expense</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Reset />
+    </div>
+  );
+};
+
+export default RecoveryPage;
